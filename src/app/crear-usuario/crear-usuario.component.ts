2ad0459056ab53e75995cc40bbfd61f70fed3567
Ruti890 <jdavid10hoyos@gmail.com>
2024-10-21 23:56:30 -0500

    first commit

--- src/app/crear-usuario/crear-usuario.component.ts
+++ src/app/crear-usuario/crear-usuario.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,29 +9,22 @@ import { CommonModule } from '@angular/common';
   styleUrl: './crear-usuario.component.css'
 })
 export class CrearUsuarioComponent {
-  title = "Crear Usuario";
   usuarioForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.usuarioForm = this.fb.group({
       nombres: ['', Validators.required],
       apellidos: ['', Validators.required],
-      identificacion: ['', [Validators.required, Validators.pattern('^[0-9]{1,10}$')]],
+      identificacion: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.maxLength(10)]],
       fechaNacimiento: ['', Validators.required],
       genero: ['', Validators.required],
       pais: ['', Validators.required]
     });
   }
+
   onSubmit() {
     if (this.usuarioForm.valid) {
       alert('Formulario enviado exitosamente');
-      this.usuarioForm.reset();
-    } else {
-      alert('Por favor complete todos los campos correctamente.');
     }
   }
 }
-
-
-
-
